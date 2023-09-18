772f4153deb2e0a4900aba55f69790c02c420bc3
Gabriel Castro <gaboj0531@gmail.com>
2023-09-18 10:40:00 -0400

    chore: changes for deploy

--- project/modules/home/ts/views/index.tsx
+++ project/modules/home/ts/views/index.tsx
@@ -5,6 +5,7 @@ import { Main } from "project/main";
 import { Footer } from "project/footer";
 export /*bundle*/
 function Page(): JSX.Element {
+  //code
   const TITLE__HEADER = "Modern interior";
   return (
     <div className="page__container">
